29629acd1fcb16d5a63a4ca7396ac90a180b44a5
RicoAng323 <novanrico5@gmail.com>
2025-10-30 11:27:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const tombolMelihat = document.getElementByid('sapaButton')
-tombolMelihat.addEventListener('click', funtion) { 
-  alert(Halo! Terimakasih sudah berkunjung!')
+// Ditemukan: document.getElementById (I besar)
+const tombolMelihat = document.getElementById('sapaButton');
+
+// eventListener akan menjalankan 'function' (dengan c dan ())
+tombolMelihat.addEventListener('click', function () { 
+  // Pesan alert sudah dibungkus tanda kutip
+  alert('Halo! Terimakasih sudah berkunjung!');
 });
